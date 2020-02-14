17250f8dff48da8324aa7d51e6eab162ec3abfdc
Kevin <kevin@Kevinui-iMac.local>
2020-02-14 19:18:11 +0900

    project init

---
+++ src/shared/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom'
+import { Home } from '../pages'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Route exact path="/" component={Home} />
+    </div>
+  );
+}
+
+export default App;
